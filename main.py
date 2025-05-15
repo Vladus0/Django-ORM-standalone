3351ce3f69c02b2ac8f8e6383af04e974d5ef01c
Vladus0 <118992340+Vladus0@users.noreply.github.com>
2025-05-15 17:52:19 +0400

    добавил подсчет пропусков

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-
 import django
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -8,5 +8,8 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+    
+    print(f"Кол-во всех пропусков: {len(passcards)}")
+    print(f"Кол-во активных пропусков: {len(active_passcards)}")
